7d23b3e3019b90972c71d5eec9887c3ca4405ca5
techinezz <y.martinez.tech@gmail.com>
2023-12-29 17:52:03 -0500

    Links

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/success"
-      cancelUrl="http://localhost:3000/cancel"
+      successUrl="https://kickx.vercel.app/success"
+      cancelUrl="https://kickx.vercel.app/cancel"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
